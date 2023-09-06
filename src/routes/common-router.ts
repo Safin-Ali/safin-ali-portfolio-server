decc6c8f587a2b6815fb47a81543fa244b422a09
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-06 14:02:55 +0600

    for all routes middleware fixed

--- src/routes/common-router.ts
+++ src/routes/common-router.ts
@@ -4,6 +4,6 @@ import configRouter from 'src/utilities/config-router';
 
 export const router = Router();
 
-router.get('/api/*',rootRouteHandler);
+router.all('/api/*',rootRouteHandler);
 
 export default configRouter(['/',router]);
