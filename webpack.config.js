590668a04c0da353322cdd078e6712e74a885471
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-09-06 13:53:28 +0600

    update babel,ts,webpack config features

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,38 @@
-const path = require('node:path');
+const path = require('path');
+const ts_config_webpack_path = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, 'src', 'index.ts'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].bundle.js',
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all',
-                },
-            },
-        },
-    },
-        module: {
-            rules: [
-                { test: /\.ts?$/, use: 'babel-loader', exclude: /node_modules/, }
-            ]
-        },
-        resolve: {
-            extensions: ['.ts', '.js'],
-        },
-    }
+	mode: 'production',
+	target:'node',
+	entry: {
+		main:path.resolve(__dirname, 'src', 'index.ts'),
+	},
+	output: {
+		clean:true,
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].bundle.js',
+	},
+	optimization: {
+		usedExports:true,
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendor',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	module: {
+		rules: [
+			{ test: /\.ts?$/, use: 'babel-loader', exclude: /node_modules/ }
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+		plugins:[
+			new ts_config_webpack_path()
+		]
+	},
+};
