dddae9047ce2c854b22222e4fee1012a3a206dd6
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-01 14:24:54 +0600

    redirect to the /api path and root path handler created

--- src/routes/common-router.ts
+++ src/routes/common-router.ts
@@ -1,9 +1,28 @@
 import { rootRouteHandler } from '@controllers/root-page';
 import configRouter from '@utilities/config-router';
-import {Router} from 'express';
+import { Router } from 'express';
+import { Response } from 'express';
 
 export const router = Router();
 
-router.all('/api/*',rootRouteHandler);
+/**
+ * Handles GET requests to the root path and redirects to '/api'.
+ */
+router.get('/', (_, res: Response) => {
+	return res.redirect(301, '/api');
+});
 
-export default configRouter(['/',router]);
+/**
+ * Handles GET requests to the '/api' path and delegates to 'rootRouteHandler'.
+ */
+router.get('/api', rootRouteHandler);
+
+/**
+ * Handles all HTTP methods (GET, POST, PUT, DELETE, etc.) for paths under '/api' and delegates to 'rootRouteHandler'.
+ */
+router.all('/api/*', rootRouteHandler);
+
+/**
+ * Exported default configuration for the router.
+ */
+export default configRouter(['', router]);
