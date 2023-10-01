227a9a4a52b0984c0a128c3a3736aeff681f72c1
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-01 20:23:32 +0600

    get project data by tech anme and use try catch

--- src/controllers/projetcs-controller.ts
+++ src/controllers/projetcs-controller.ts
@@ -1,7 +1,24 @@
 import { ProjectsModel } from '@model/projects-schema';
-import { Response} from 'express';
+import { Response, Request } from 'express';
 
 export async function getProjects(_:unknown, res: Response) {
-	const result = await ProjectsModel.find();
-	return res.send(result);
+	try{
+		const result = await ProjectsModel.find();
+		return res.send(result);
+	} catch {
+		return res.status(500).send('Internal Server Error');
+	}
+}
+
+export async function getNProjects(req: Request, res: Response) {
+	try{
+		const result = await ProjectsModel.find({
+			projectTech: {
+				$in: [req.query.category]
+			}
+		});
+		return res.send(result);
+	} catch {
+		return res.status(500).send('Internal Server Error');
+	}
 }
