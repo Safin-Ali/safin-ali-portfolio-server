b2edc9d7d58ae1691b7477e94da419da63cc0b6f
Safin Ali <108370873+Safin-Ali@users.noreply.github.com>
2023-10-06 15:16:51 +0600

    created sendEmail handler with req params CustomBody type

---
+++ src/controllers/smtp-controller.ts
@@ -0,0 +1,29 @@
+import { Response } from 'express';
+import { Custom_Requst, EmailType } from '@custom-types/routes.d';
+import inDevMode from '@utilities/developmet-mode';
+import logger from '@utilities/color-logger';
+import { transport } from '@utilities/common-utilities';
+
+/**
+ *
+ * @param req - The Express.js request object with custom body object.
+ * @param res - The Express.js response object.
+ */
+
+export async function sendEmail(req: Custom_Requst<EmailType>, res: Response) {
+
+	const {emailDesc,emailSub,senderEmail,senderName} = req.body;
+
+	try {
+		await transport.sendMail({
+			from:`"${senderName}" <${senderEmail}>`,
+			to:'safin.ali.7205@gmail.com',
+			subject:emailSub,
+			text: emailDesc,
+		});
+		res.status(200).send('success');
+	} catch (e: any) {
+		inDevMode(() => logger.error(e.message));
+		res.status(500).send('error');
+	}
+}
--- src/types/routes.d.ts
+++ src/types/routes.d.ts
@@ -5,4 +5,11 @@ export interface Custom_Requst <T> extends Request {
 	body:T
 }
 
+export interface EmailType {
+	emailSub:string
+	senderName:string,
+	senderEmail:string,
+	emailDesc:string,
+}
+
 export type CustomRouter = [string, createRouter.Router];
