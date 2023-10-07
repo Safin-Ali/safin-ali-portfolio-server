bcb970b049a3e63c3a2631c0a79865c0b916b81b
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-07 19:26:37 +0600

    added feature senEmail handler to send email-template HTML

--- src/controllers/smtp-controller.ts
+++ src/controllers/smtp-controller.ts
@@ -1,20 +1,35 @@
 import { CustomRequestBodyType, EmailType } from '@custom-types/routes.d';
 import inDevMode from '@utilities/developmet-mode';
 import logger from '@utilities/color-logger';
-import { routeHandler, transport } from '@utilities/common-utilities';
+import { readFileAsync, resolveWithRoot, routeHandler, transport } from '@utilities/common-utilities';
+import ejs from 'ejs';
 
-export const sendEmail = routeHandler<Promise<void>,CustomRequestBodyType<EmailType>>(async (req, res) => {
+export const sendEmail = routeHandler<Promise<void>, CustomRequestBodyType<EmailType>>(async (req, res) => {
 
-	const {emailDesc,emailSub,senderEmail,senderName} = req.body;
+	const monthsShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	const dateObj = new Date();
+
+	const { emailDesc, emailSub, senderEmail, senderName } = req.body;
+
+	const ISODate = `${monthsShort[dateObj.getMonth() + 1]} ${dateObj.getMonth()} ${dateObj.getFullYear()}`;
 
 	try {
-		await transport.sendMail({
-			from:`"${senderName}" <${senderEmail}>`,
-			to:'safin.ali.7205@gmail.com',
-			subject:emailSub,
-			text: emailDesc,
+		readFileAsync(resolveWithRoot('template', 'email-template.ejs'), async (data) => {
+			await transport.sendMail({
+				from: `'${senderName}' <${senderEmail}>`,
+				to: 'safin.ali.7205@gmail.com',
+				subject: `Email from Safin-Ali Portfolio`,
+				html: ejs.render(data, {
+					date: ISODate,
+					senderEmail,
+					emailDesc,
+					emailSub,
+					senderName
+				})
+			});
+			res.status(200).json('success');
 		});
-		res.status(200).json('success');
 	} catch (e: any) {
 		inDevMode(() => logger.error(e.message));
 		res.status(500).json('error');
