441b90547a284b80c559c2b804525f32a3f46e27
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-09 02:52:07 +0600

    removed two resolveWithRoot and readAsync utilities

--- src/utilities/common-utilities.ts
+++ src/utilities/common-utilities.ts
@@ -1,19 +1,8 @@
-import { resolve } from 'path';
-import { PathOrFileDescriptor, readFile } from 'fs';
 import inDevMode from './developmet-mode';
 import logger from './color-logger';
 import { createTransport } from 'nodemailer';
 import { my_email, my_email_pass } from '@config/env-var';
 import { RouteHandlerType } from '@custom-types/routes.d';
-/**
- * Resolves a path relative to the root directory.
- * @param {...string} path - The path segments to resolve.
- * @returns {string} The resolved path.
- */
-export const resolveWithRoot = (...path: string[]) => {
-	return resolve(__dirname, '..', ...path);
-};
-
 /**
  * create nodemailer transport
  */
@@ -25,28 +14,6 @@ export const transport = createTransport({
 		pass: my_email_pass
 	}
 });
-
-/**
- *
- * @description readFile asynchronously with `utf-8` encoding
- * @param path - accept file path.
- * @param reject - accept `callback function` which will be error or data handler.
- * @param resolve - that is `optional` params accept `callback function` which will be only data handler
- * @overload `Node: If use two Argument then second Argument will be considered as data handler`
- *
- */
-
-export function readFileAsync(path: PathOrFileDescriptor, reject: ((e: string) => void), resolve?: ((d: string) => void)) {
-	readFile(path, 'utf-8', (err, data) => {
-		if (typeof resolve === 'undefined') {
-			if (err) return inDevMode(() => logger.error(err.message));
-			reject(data);
-		} else {
-			if (err) return reject(err.message);
-			resolve(data);
-		}
-	});
-}
 
 
 /**
