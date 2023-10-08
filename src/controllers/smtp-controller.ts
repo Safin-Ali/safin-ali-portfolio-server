f5ea03d0badd52cd4ced7647cfa86439c286b976
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-09 02:53:03 +0600

    the ejs file path resolving or server no such file error fixed

--- src/controllers/smtp-controller.ts
+++ src/controllers/smtp-controller.ts
@@ -1,8 +1,10 @@
 import { CustomRequestBodyType, EmailType } from '@custom-types/routes.d';
 import inDevMode from '@utilities/developmet-mode';
 import logger from '@utilities/color-logger';
-import { readFileAsync, resolveWithRoot, routeHandler, transport } from '@utilities/common-utilities';
-import ejs from 'ejs';
+import { routeHandler, transport } from '@utilities/common-utilities';
+import {render} from 'ejs';
+import {resolve} from 'path';
+import {readFileSync} from 'fs';
 
 export const sendEmail = routeHandler<Promise<void>, CustomRequestBodyType<EmailType>>(async (req, res) => {
 
@@ -15,22 +17,23 @@ export const sendEmail = routeHandler<Promise<void>, CustomRequestBodyType<Email
 	const ISODate = `${monthsShort[dateObj.getMonth() + 1]} ${dateObj.getMonth()} ${dateObj.getFullYear()}`;
 
 	try {
-		readFileAsync(resolveWithRoot('template', 'email-template.ejs'), async (data) => {
-			await transport.sendMail({
-				from: `'${senderName}' <${senderEmail}>`,
-				to: 'safin.ali.7205@gmail.com',
-				subject: `Email from Safin-Ali Portfolio`,
-				html: ejs.render(data, {
-					date: ISODate,
-					senderEmail,
-					emailDesc,
-					emailSub,
-					senderName
-				})
-			});
-			res.status(200).json('success');
+		const emailHtml = readFileSync(resolve(__dirname,'..','template', 'email-template.ejs'),'utf-8');
+		await transport.sendMail({
+			from: `'${senderName}' <${senderEmail}>`,
+			to: 'safin.ali.7205@gmail.com',
+			subject: `Email from Safin-Ali Portfolio`,
+			html: render(emailHtml, {
+				date: ISODate,
+				senderEmail,
+				emailDesc,
+				emailSub,
+				senderName
+			})
 		});
+		res.status(200).json('success');
 	} catch (e: any) {
+		console.log(e.message);
+
 		inDevMode(() => logger.error(e.message));
 		res.status(500).json('error');
 	}
