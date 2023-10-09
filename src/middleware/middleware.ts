0a5948aa1c68859e39324073f78f3b17288c3ad9
Safin-Ali <safin.ali.7205@gmail.com>
2023-10-09 17:13:50 +0600

    set to EJS by default render engine

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,8 +1,15 @@
 import { Application, } from 'express';
 import CORS from './CORS';
 import {json} from 'express';
+import {resolve} from 'path';
 
 const initMiddleware = (app:Application) => {
+
+	// replace default rendering views directory to the template directory
+	app.set('views',resolve(__dirname,'..','template'));
+
+	// set EJS default render engine
+	app.set('view engine','ejs');
 
 	// for JSON pharser
 	app.use(json());
