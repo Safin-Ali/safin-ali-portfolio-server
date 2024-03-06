c61a9c23ae5ac773b89f679a951c07b4694a9329
Safin Ali <safin.ali.7205@gmail.com>
2024-03-06 16:27:58 +0600

    feat: :tada: init root route and ejs for default rendering engine

---
+++ src/controllers/root-controller.ts
@@ -0,0 +1,12 @@
+/**
+ * Handles requests to the root route ("/") and sends an HTML response.
+ *
+ * @param {import('express').Request} _ - The Express.js request object.
+ * @param {import('express').Response} res - The Express.js response object.
+ */
+
+import { routeHandler } from 'utils/common-utils';
+
+export const rootRouteHandler = routeHandler((_,res)=>{
+	res.render('root-page');
+});
