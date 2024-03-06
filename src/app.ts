3fc5ba3a96e520e1dc23b1f830e8b911f0751f2a
Safin Ali <safin.ali.7205@gmail.com>
2024-03-06 16:27:58 +0600

    feat: :tada: init root route and ejs for default rendering engine

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import expres,{json} from 'express';
 import {config}  from 'dotenv';
 import cors from 'cors';
+import { rootRouteHandler } from '@controllers/root-controller';
+import {resolve} from 'path';
 
 config();
 const port = process.env.PORT || 5000;
 
 const app = expres();
+
+// replace default rendering views directory to the template directory
+app.set('views',resolve(__dirname,'template'));
+
+// set EJS default render engine
+app.set('view engine','ejs');
 
 app.use(cors({
 	origin:'https://safin-ali.vercel.app',
@@ -14,10 +22,7 @@ app.use(cors({
 
 app.use(json());
 
-app.get('/',(req,res) => {
-	res.send();
-})
-
+app.get('/api',rootRouteHandler);
 
 app.listen(port, () => {
 	console.log(`server run on ${port}`)
